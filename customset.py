979da8226e7bb8b88560ee1fbab5f9e557e12689
Izzy Hymas <izzyhymas@gmail.com>
2024-02-10 18:42:36 -0700

    Add Functions and Print

--- customset.py
+++ customset.py
@@ -1,22 +1,42 @@
 class CustomSet:
 
     def __init__(self):
-        pass
+        self.items = []
 
     def add(self, item: str)-> set:
-        pass
+        if item not in self.items:
+            self.items.append(item)
 
     def remove(self, item: str)-> set:
-        pass
+        if item in self.items:
+            self.items.remove(item)
+        else:
+            raise KeyError
 
     def as_list(self)-> list:
-        return
+        return self.items.copy()
 
     def clear(self)-> list:
-        return
+        self.items.clear()
 
 def main():
-    pass
+    my_set = CustomSet()
+    my_set.add("item 1")
+    my_set.add("item 2")
+    my_set.add("item 1")
+
+    print(my_set.as_list())
+    
+    my_set.remove("item 2")
+    print(my_set.as_list())
+
+    try:
+        my_set.remove("item 3")
+    except KeyError:
+        print("Item not removed, moving foward")
+
+    my_set.clear()
+    print(my_set.as_list())
 
 if __name__ == "__main__":
     main()
