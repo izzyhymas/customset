3e3e7c2a2d06a6e725b0254ac4e8d50128e4315a
Izzy Hymas <izzyhymas@gmail.com>
2024-02-10 18:31:21 -0700

    Add CustomSet Class and Main

---
+++ customset.py
@@ -0,0 +1,22 @@
+class CustomSet:
+
+    def __init__(self):
+        pass
+
+    def add(self, item: str)-> set:
+        pass
+
+    def remove(self, item: str)-> set:
+        pass
+
+    def as_list(self)-> list:
+        return
+
+    def clear(self)-> list:
+        return
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
